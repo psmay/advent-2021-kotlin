bfb82c3d4d72c76dfa62c960d2974064602bbc19
Peter S. May <me@psmay.com>
2021-12-06 10:22:04 -0500

    Added a UseLinesSource helper interface that allows us to use `useLines()` on a string sequence or interface in the same way it is used on a `File` or `Reader`.

--- src/test/kotlin/com/psmay/exp/advent/helpers/Helpers.kt
+++ src/test/kotlin/com/psmay/exp/advent/helpers/Helpers.kt
@@ -1,5 +1,40 @@
 package com.psmay.exp.advent.helpers
 
 import java.io.File
+import java.io.Reader
 
 fun getTextFile(name: String) = File("src/test/resources", "$name.txt")
+
+fun getTextLineSource(name: String) = getTextFile(name).asUseLinesSource()
+
+interface UseLinesSource {
+    fun <T> useLines(block: (Sequence<String>) -> T): T
+}
+
+fun File.asUseLinesSource(): UseLinesSource {
+    val file = this
+    return object : UseLinesSource {
+        override fun <T> useLines(block: (Sequence<String>) -> T): T = file.useLines(block = block)
+    }
+}
+
+fun Reader.asUseLinesSource(): UseLinesSource {
+    val reader = this
+    return object : UseLinesSource {
+        override fun <T> useLines(block: (Sequence<String>) -> T): T = reader.useLines(block)
+    }
+}
+
+fun Sequence<String>.asUseLinesSource(): UseLinesSource {
+    val sequence = this
+    return object : UseLinesSource {
+        override fun <T> useLines(block: (Sequence<String>) -> T): T = block(sequence)
+    }
+}
+
+fun Iterable<String>.asUseLinesSource(): UseLinesSource {
+    val iterable = this
+    return object : UseLinesSource {
+        override fun <T> useLines(block: (Sequence<String>) -> T): T = block(iterable.asSequence())
+    }
+}
