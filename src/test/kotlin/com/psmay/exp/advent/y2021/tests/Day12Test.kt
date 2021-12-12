11f17fa7190fca18a1229f23778b7a9cb9b4eef8
Peter S. May <me@psmay.com>
2021-12-12 13:53:52 -0500

    Tests for day 12.

---
+++ src/test/kotlin/com/psmay/exp/advent/y2021/tests/Day12Test.kt
@@ -0,0 +1,166 @@
+package com.psmay.exp.advent.y2021.tests
+
+import com.psmay.exp.advent.y2021.tests.helpers.UseLinesSource
+import com.psmay.exp.advent.y2021.tests.helpers.asUseLinesSource
+import com.psmay.exp.advent.y2021.tests.helpers.getTextLineSource
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+
+internal class Day12Test {
+    private val exampleInput1 = sequenceOf(
+        "start" to "A",
+        "start" to "b",
+        "A" to "c",
+        "A" to "b",
+        "b" to "d",
+        "A" to "end",
+        "b" to "end",
+    )
+
+    private val exampleInput2 = sequenceOf(
+        "dc" to "end",
+        "HN" to "start",
+        "start" to "kj",
+        "dc" to "start",
+        "dc" to "HN",
+        "LN" to "dc",
+        "HN" to "end",
+        "kj" to "sa",
+        "kj" to "HN",
+        "kj" to "dc",
+    )
+
+    private val exampleInput3 = sequenceOf(
+        "fs" to "end",
+        "he" to "DX",
+        "fs" to "he",
+        "start" to "DX",
+        "pj" to "DX",
+        "end" to "zg",
+        "zg" to "sl",
+        "zg" to "pj",
+        "pj" to "he",
+        "RW" to "he",
+        "fs" to "DX",
+        "pj" to "RW",
+        "zg" to "RW",
+        "start" to "pj",
+        "he" to "WI",
+        "zg" to "he",
+        "pj" to "fs",
+        "start" to "RW",
+    )
+
+    private val exampleRawInput1 = sequenceOf(
+        "start-A",
+        "start-b",
+        "A-c",
+        "A-b",
+        "b-d",
+        "A-end",
+        "b-end",
+    ).asUseLinesSource()
+
+    private val exampleRawInput2 = sequenceOf(
+        "dc-end",
+        "HN-start",
+        "start-kj",
+        "dc-start",
+        "dc-HN",
+        "LN-dc",
+        "HN-end",
+        "kj-sa",
+        "kj-HN",
+        "kj-dc",
+    ).asUseLinesSource()
+
+    private val exampleRawInput3 = sequenceOf(
+        "fs-end",
+        "he-DX",
+        "fs-he",
+        "start-DX",
+        "pj-DX",
+        "end-zg",
+        "zg-sl",
+        "zg-pj",
+        "pj-he",
+        "RW-he",
+        "fs-DX",
+        "pj-RW",
+        "zg-RW",
+        "start-pj",
+        "he-WI",
+        "zg-he",
+        "pj-fs",
+        "start-RW",
+    ).asUseLinesSource()
+
+    data class TestCase(
+        val exampleInput: UseLinesSource,
+        val exampleRawInput: Sequence<Pair<String, String>>,
+        val part1Result: Int,
+        val part2Result: Int,
+    )
+
+    private val testCases = listOf(
+        TestCase(exampleRawInput1, exampleInput1, 10, -1),
+        TestCase(exampleRawInput2, exampleInput2, 19, -1),
+        TestCase(exampleRawInput3, exampleInput3, 226, -1),
+    )
+
+    private val puzzleRawInput = getTextLineSource("y2021/Day12Input")
+
+    private fun parseLine(line: String): Pair<String, String> {
+        val parts = line.split("-", limit = 3)
+        if (parts.size != 2) throw IllegalArgumentException("Edge description must have 2 parts separated by '-'.")
+        val (a, b) = parts
+        return a to b
+    }
+
+    @TestFactory
+    fun `raw input cooks to correct cooked form`() = testCases.map { (input, expectedSequence, _, _) ->
+        val expected = expectedSequence.toList()
+        dynamicTest("$input to $expected") {
+            val result = input.useLines { lines -> lines.map { parseLine(it) }.toList() }
+            Assertions.assertEquals(expected, result)
+        }
+    }
+
+    private fun part1(input: Sequence<Pair<String, String>>): Int {
+        return 0
+    }
+
+    private fun part2(input: Sequence<Pair<String, String>>): Int {
+        return 0
+    }
+
+    @TestFactory
+    fun `part1 produces sample results as expected`() = testCases.map { (input, _, expected, _) ->
+        dynamicTest("$input to $expected") {
+            val result = input.useLines { lines -> part1(lines.map { parseLine(it) }) }
+            Assertions.assertEquals(expected, result)
+        }
+    }
+
+    @TestFactory
+    fun `part2 produces sample results as expected`() = testCases.map { (input, _, _, expected) ->
+        dynamicTest("$input to $expected") {
+            val result = input.useLines { lines -> part2(lines.map { parseLine(it) }) }
+            Assertions.assertEquals(expected, result)
+        }
+    }
+
+    @Test
+    fun `part1 on puzzle input succeeds`() {
+        val result = puzzleRawInput.useLines { lines -> part1(lines.map { parseLine(it) }) }
+        println("Result: $result")
+    }
+
+    @Test
+    fun `part2 on puzzle input succeeds`() {
+        val result = puzzleRawInput.useLines { lines -> part2(lines.map { parseLine(it) }) }
+        println("Result: $result")
+    }
+}
