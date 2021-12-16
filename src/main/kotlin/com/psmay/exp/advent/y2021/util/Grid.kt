c5e8d5e36eb6143bc65063ddbce33be26b36cc6c
Peter S. May <me@psmay.com>
2021-12-16 15:42:13 -0500

    Grids can now be concatenated.

--- src/main/kotlin/com/psmay/exp/advent/y2021/util/Grid.kt
+++ src/main/kotlin/com/psmay/exp/advent/y2021/util/Grid.kt
@@ -77,3 +77,45 @@ fun Grid<Boolean>.plotToStrings(forFalse: String = ".", forTrue: String = "#"): 
         row.joinToString("") { if (it) forTrue else forFalse }
     }
 }
+
+fun <T> Iterable<Grid<T>>.concatenateHorizontally(): Grid<T> {
+    val grids = this.toList()
+    if (grids.isEmpty()) {
+        return Grid(listOf())
+    } else if (grids.size == 1) {
+        return grids[0]
+    }
+
+    run {
+        val heights = grids.map { it.height }.distinct()
+        if (heights.size > 1) {
+            throw IllegalArgumentException("Grids to concatenate must have same height.")
+        }
+    }
+
+    val rowIndices = grids[0].rowIndices
+
+    val rows = rowIndices.map { rowIndex -> grids.map { grid -> grid.rows[rowIndex] }.flatten() }
+
+    return Grid(rows)
+}
+
+fun <T> Iterable<Grid<T>>.concatenateVertically(): Grid<T> {
+    val grids = this.toList()
+    if (grids.isEmpty()) {
+        return Grid(listOf())
+    } else if (grids.size == 1) {
+        return grids[0]
+    }
+
+    run {
+        val widths = grids.map { it.width }.distinct()
+        if (widths.size > 1) {
+            throw IllegalArgumentException("Grids to concatenate must have same width.")
+        }
+    }
+
+    val rows = grids.flatMap { it.rows }
+
+    return Grid(rows)
+}
