1d4e7aca4e635ef095b91a26fba7af0c06db1a8b
Peter S. May <me@psmay.com>
2021-12-17 21:21:10 -0500

    Updated tests for day 16 part 2.

--- src/test/kotlin/com/psmay/exp/advent/y2021/tests/Day16Test.kt
+++ src/test/kotlin/com/psmay/exp/advent/y2021/tests/Day16Test.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 
 internal class Day16Test {
-    private val exampleInputs = listOf(
+    private val exampleInputsPart1 = listOf(
         sequenceOf(0xD, 0x2, 0xF, 0xE, 0x2, 0x8),
         sequenceOf(0x3, 0x8, 0x0, 0x0, 0x6, 0xF, 0x4, 0x5, 0x2, 0x9, 0x1, 0x2, 0x0, 0x0),
         sequenceOf(0xE, 0xE, 0x0, 0x0, 0xD, 0x4, 0x0, 0xC, 0x8, 0x2, 0x3, 0x0, 0x6, 0x0),
@@ -102,7 +102,43 @@ internal class Day16Test {
             0x0),
     )
 
-    private val exampleRawInputs = listOf(
+    private val exampleInputsPart2 = listOf(
+        sequenceOf(0xC, 0x2, 0x0, 0x0, 0xB, 0x4, 0x0, 0xA, 0x8, 0x2),
+        sequenceOf(0x0, 0x4, 0x0, 0x0, 0x5, 0xA, 0xC, 0x3, 0x3, 0x8, 0x9, 0x0),
+        sequenceOf(0x8, 0x8, 0x0, 0x0, 0x8, 0x6, 0xC, 0x3, 0xE, 0x8, 0x8, 0x1, 0x1, 0x2),
+        sequenceOf(0xC, 0xE, 0x0, 0x0, 0xC, 0x4, 0x3, 0xD, 0x8, 0x8, 0x1, 0x1, 0x2, 0x0),
+        sequenceOf(0xD, 0x8, 0x0, 0x0, 0x5, 0xA, 0xC, 0x2, 0xA, 0x8, 0xF, 0x0),
+        sequenceOf(0xF, 0x6, 0x0, 0x0, 0xB, 0xC, 0x2, 0xD, 0x8, 0xF),
+        sequenceOf(0x9, 0xC, 0x0, 0x0, 0x5, 0xA, 0xC, 0x2, 0xF, 0x8, 0xF, 0x0),
+        sequenceOf(0x9,
+            0xC,
+            0x0,
+            0x1,
+            0x4,
+            0x1,
+            0x0,
+            0x8,
+            0x0,
+            0x2,
+            0x5,
+            0x0,
+            0x3,
+            0x2,
+            0x0,
+            0xF,
+            0x1,
+            0x8,
+            0x0,
+            0x2,
+            0x1,
+            0x0,
+            0x4,
+            0xA,
+            0x0,
+            0x8),
+    )
+
+    private val exampleRawInputsPart1 = listOf(
         sequenceOf("D2FE28").asUseLinesSource(),
         sequenceOf("38006F45291200").asUseLinesSource(),
         sequenceOf("EE00D40C823060").asUseLinesSource(),
@@ -112,21 +148,35 @@ internal class Day16Test {
         sequenceOf("A0016C880162017C3686B18A3D4780").asUseLinesSource(),
     )
 
+    private val exampleRawInputsPart2 = listOf(
+        sequenceOf("C200B40A82").asUseLinesSource(),
+        sequenceOf("04005AC33890").asUseLinesSource(),
+        sequenceOf("880086C3E88112").asUseLinesSource(),
+        sequenceOf("CE00C43D881120").asUseLinesSource(),
+        sequenceOf("D8005AC2A8F0").asUseLinesSource(),
+        sequenceOf("F600BC2D8F").asUseLinesSource(),
+        sequenceOf("9C005AC2F8F0").asUseLinesSource(),
+        sequenceOf("9C0141080250320F1802104A08").asUseLinesSource(),
+    )
     private val examplePart1Results = listOf(6, 9, 14, 16, 12, 23, 31)
 
-    private val examplePart2Results = listOf(-1, -1, -1, -1, -1, -1, -1)
+    private val examplePart2Results = listOf(3, 54, 7, 9, 1, 0, 0, 1)
 
     data class TestCase(
         val exampleRawInput: UseLinesSource,
         val exampleInput: Sequence<Int>,
-        val part1Result: Int,
-        val part2Result: Int,
+        val result: Int,
     )
 
-    private val testCases = exampleRawInputs.indices.map {
-        TestCase(exampleRawInputs[it],
-            exampleInputs[it],
-            examplePart1Results[it],
+    private val testCasesPart1 = exampleRawInputsPart1.indices.map {
+        TestCase(exampleRawInputsPart1[it],
+            exampleInputsPart1[it],
+            examplePart1Results[it])
+    }
+
+    private val testCasesPart2 = exampleRawInputsPart2.indices.map {
+        TestCase(exampleRawInputsPart2[it],
+            exampleInputsPart2[it],
             examplePart2Results[it])
     }
 
@@ -137,7 +187,7 @@ internal class Day16Test {
     private fun parseAll(lines: Sequence<String>) = lines.map { parseLine(it) }.flatten()
 
     @TestFactory
-    fun `raw input cooks to correct cooked form`() = testCases.map { (actualRaw, expected, _, _) ->
+    fun `raw input cooks to correct cooked form`() = (testCasesPart1 + testCasesPart2).map { (actualRaw, expected, _) ->
         dynamicTest("$actualRaw to $expected") {
             val result = actualRaw.useLines { parseAll(it) }
             Assertions.assertEquals(expected.toList(), result.toList())
@@ -160,7 +210,7 @@ internal class Day16Test {
     }
 
     @TestFactory
-    fun `part1 produces sample results as expected`() = testCases.map { (input, _, expected, _) ->
+    fun `part1 produces sample results as expected`() = testCasesPart1.map { (input, _, expected) ->
         dynamicTest("$input to $expected") {
             val result = input.useLines { lines -> part1(parseAll(lines)) }
             Assertions.assertEquals(expected, result)
@@ -168,7 +218,7 @@ internal class Day16Test {
     }
 
     @TestFactory
-    fun `part2 produces sample results as expected`() = testCases.map { (input, _, _, expected) ->
+    fun `part2 produces sample results as expected`() = testCasesPart2.map { (input, _, expected) ->
         dynamicTest("$input to $expected") {
             val result = input.useLines { lines -> part2(parseAll(lines)) }
             Assertions.assertEquals(expected, result)
