42a4aa4ab0eecb8534e736363597f17f97892a35
Peter S. May <me@psmay.com>
2021-12-18 11:50:59 -0500

    Tests for day 17.

--- src/main/kotlin/com/psmay/exp/advent/y2021/day17/Day17.kt
+++ src/main/kotlin/com/psmay/exp/advent/y2021/day17/Day17.kt
@@ -1,2 +1,18 @@
 package com.psmay.exp.advent.y2021.day17
 
+data class TargetArea(val xRange: IntRange, val yRange: IntRange)
+
+private val targetAreaRegex = run {
+    val number = """(?:-?[0-9]+)"""
+    val thru = """(?:\.\.)"""
+    val targetAreaPattern = """^target area: x=($number)$thru($number), y=($number)$thru($number)$"""
+    targetAreaPattern.toRegex()
+}
+
+fun parseTargetAreaOrNull(value: String): TargetArea? {
+    val (x0, x1, y0, y1) = targetAreaRegex.matchEntire(value)?.destructured ?: return null
+    return TargetArea(x0.toInt()..x1.toInt(), y0.toInt()..y1.toInt())
+}
+
+fun parseTargetArea(value: String): TargetArea =
+    parseTargetAreaOrNull(value) ?: throw IllegalArgumentException("Input does not match expected target area format.")
---
+++ src/test/kotlin/com/psmay/exp/advent/y2021/tests/Day17Test.kt
@@ -0,0 +1,83 @@
+package com.psmay.exp.advent.y2021.tests
+
+import com.psmay.exp.advent.y2021.day17.TargetArea
+import com.psmay.exp.advent.y2021.day17.parseTargetArea
+import com.psmay.exp.advent.y2021.tests.helpers.UseLinesSource
+import com.psmay.exp.advent.y2021.tests.helpers.asUseLinesSource
+import com.psmay.exp.advent.y2021.tests.helpers.getTextLineSource
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+
+internal class Day17Test {
+    private val exampleInput1 = listOf(
+        TargetArea(20..30, -10..-5)
+    )
+
+    private val exampleRawInput1 = sequenceOf(
+        "target area: x=20..30, y=-10..-5"
+    ).asUseLinesSource()
+
+    data class TestCase(
+        val exampleRawInput: UseLinesSource,
+        val exampleInput: List<TargetArea>,
+        val part1Result: Int,
+        val part2Result: Int,
+    )
+
+    private val testCases = listOf(
+        TestCase(exampleRawInput1, exampleInput1, 445, -1)
+    )
+
+    private val puzzleRawInput = getTextLineSource("y2021/Day17Input")
+
+    private fun parseLine(line: String): TargetArea = parseTargetArea(line)
+
+    private fun parseAll(lines: Sequence<String>) = lines.map { parseLine(it) }.toList()
+
+    @TestFactory
+    fun `raw input cooks to correct cooked form`() = testCases.map { (actualRaw, expected, _, _) ->
+        dynamicTest("$actualRaw to $expected") {
+            val result = actualRaw.useLines { parseAll(it) }
+            Assertions.assertEquals(expected, result)
+        }
+    }
+
+    private fun part1(input: List<TargetArea>): Int {
+        return 0
+    }
+
+    private fun part2(input: List<TargetArea>): Int {
+        return 0
+    }
+
+    @TestFactory
+    fun `part1 produces sample results as expected`() = testCases.map { (input, _, expected, _) ->
+        dynamicTest("$input to $expected") {
+            val result = input.useLines { lines -> part1(parseAll(lines)) }
+            Assertions.assertEquals(expected, result)
+        }
+    }
+
+    @TestFactory
+    fun `part2 produces sample results as expected`() = testCases.map { (input, _, _, expected) ->
+        dynamicTest("$input to $expected") {
+            val result = input.useLines { lines -> part2(parseAll(lines)) }
+            Assertions.assertEquals(expected, result)
+        }
+    }
+
+    @Test
+    fun `part1 on puzzle input succeeds`() {
+        val result = puzzleRawInput.useLines { lines -> part1(parseAll(lines)) }
+        println("Result: $result")
+    }
+
+    @Test
+    fun `part2 on puzzle input succeeds`() {
+        val result = puzzleRawInput.useLines { lines -> part2(parseAll(lines)) }
+        println("Result: $result")
+    }
+}
+
